c024538a1b76fdc771ed4c66e951e07dc6a2183f
trunghieu25071 <85150946+trunghieu25071@users.noreply.github.com>
2022-11-10 12:31:00 +0700

    Update test_acunetix.py

--- test_acunetix.py
+++ test_acunetix.py
@@ -2,8 +2,9 @@ import threading
 from queue import Queue
 import json
 import requests
-import requests.packages.urllib3
+import urllib3
 import time
+urllib3.disable_warnings()
 list_url = Queue()
 list_url.put('http://fbox-fw.fpt.vn/')
 list_url.put('http://fti.fpt.vn/')
@@ -24,15 +25,16 @@ class myThread(threading.Thread):
     def __init__(self, threadID, ip, apikey):
         threading.Thread.__init__(self)
         self.threadID = threadID	
-        self.tarurl = "https://"+ip+":3443/"
+        self.tarurl = "https://"+ip+":3443"
         self.headers = {"X-Auth":apikey,"content-type": "application/json"}	
         self.url = ''
         self.list_processing = []
     def run(self):
         while( not list_url.full() ):
-            process = self.checkProcess()
+            self.checkProcess()
+            process = len(self.list_processing)
             if process >=3 :
-                time.sleep(300)
+                time.sleep(6)
             else : 
                 self.url = list_url.get()
                 self.list_processing.append(self.startscan(self.addtask()))
@@ -46,7 +48,7 @@ class myThread(threading.Thread):
             print(str(e))
             return
     def startscan(self,target_id):
-        data = {"target_id":target_id,"profile_id":"11111111-1111-1111-1111-111111111111","schedule": {"disable": False,"start_date":None,"time_sensitive": False}}
+        data = {"target_id":target_id,"profile_id":"11111111-1111-1111-1111-111111111117","schedule": {"disable": False,"start_date":None,"time_sensitive": False}}
         try:
             response = requests.post(self.tarurl+"/api/v1/scans",data=json.dumps(data),headers=self.headers,timeout=30,verify=False)
             result = json.loads(response.content)
@@ -57,15 +59,18 @@ class myThread(threading.Thread):
     def checkProcess(self) :
         for scan_id in self.list_processing : 
             try:
-                response = requests.get(self.tarurl+"/api/v1/scans/"+str(scan_id),headers=self.headers,timeout=30,verify=False)
+                response = requests.get(self.tarurl + '/api/v1/scans/' + str(scan_id), headers = self.headers, verify=False)
+                print(self.tarurl+'/api/v1/scans/'+ str(scan_id))
+                print(self.headers)
                 result = json.loads(response.content)
+                print(result)
                 status = result['current_session']['status']
                 if status == "completed":
                     self.list_processing.remove(scan_id)
             except Exception as e:
                 print(str(e))
                 return
-        return len(self.list_processing)
+        return
 threads = []
 thread1 = myThread(1, "192.168.233.130","1986ad8c0a5b3df4d7028d5f3c06e936c0d5fcab7ab93418d84681370004db207")
 thread1.start()
