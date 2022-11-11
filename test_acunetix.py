2e351994fc36e717d300ea7a642a88a04290de5d
trunghieu25071 <85150946+trunghieu25071@users.noreply.github.com>
2022-11-11 08:38:46 +0700

    Update test_acunetix.py

--- test_acunetix.py
+++ test_acunetix.py
@@ -4,6 +4,7 @@ import json
 import requests
 import urllib3
 import time
+import sys
 urllib3.disable_warnings()
 list_url = Queue()
 list_url.put('http://fbox-fw.fpt.vn/')
@@ -38,7 +39,7 @@ class myThread(threading.Thread):
             else : 
                 self.url = list_url.get()
                 self.list_processing.append(self.startscan(self.addtask()))
-    def addtask(self):
+    def addtask(self):#them target
         data = {"address":self.url,"description":self.url,"criticality":"10"}
         try:
             response = requests.post(self.tarurl+"/api/v1/targets",data=json.dumps(data),headers=self.headers,timeout=30,verify=False)
@@ -47,7 +48,7 @@ class myThread(threading.Thread):
         except Exception as e:
             print(str(e))
             return
-    def startscan(self,target_id):
+    def startscan(self,target_id):#bat dau scan target
         data = {"target_id":target_id,"profile_id":"11111111-1111-1111-1111-111111111117","schedule": {"disable": False,"start_date":None,"time_sensitive": False}}
         try:
             response = requests.post(self.tarurl+"/api/v1/scans",data=json.dumps(data),headers=self.headers,timeout=30,verify=False)
@@ -56,7 +57,50 @@ class myThread(threading.Thread):
         except Exception as e:
             print(str(e))
             return  
-    def checkProcess(self) :
+    def generated_report(self,scan_id,target):#report sang 1 file xml 
+        data = {"template_id": "21111111-1111-1111-1111-111111111111","source": {"list_type": "scans", "id_list":[scan_id]}}
+        try:
+            response = requests.post(self.tarurl + "/api/v1/reports", data=json.dumps(data), headers=self.headers, verify=False)
+            report_url = self.tarurl.strip('/') + response.headers['Location']
+            requests.get(str(report_url),headers=self.headers, verify=False)
+            while True:
+                report = self.get_report(response.headers['Location'])
+                if not report:
+                    time.sleep(5)
+                elif report:
+                    break
+            if(not os.path.exists("reports")):
+                os.mkdir("reports")
+                
+            report = requests.get(self.tarurl + report,headers=self.headers, verify=False,timeout=120)
+            
+            filename = str(target.strip('/').split('://')[1]).replace('.','_').replace('/','-')
+            file = "reports/" + filename + "%s.xml" % time.strftime("%Y-%m-%d-%H-%M", time.localtime(time.time()))
+            with open(file, "wb") as f:
+                f.write(report.content)
+            print("[INFO] %s report have %s.xml is generated successfully" % (target,filename))
+        except Exception as e:
+            raise e
+        finally:
+            self.delete_report(response.headers['Location'])
+    def get_report(self,reportid):
+        res = requests.get(url=self.tarurl + reportid, timeout=10, verify=False, headers=self.headers)
+        try:
+            report_url = res.json()['download'][0]
+            return report_url
+        except Exception as e:
+            return False
+    def delete_scan(self,scan_id):#xoa target sau khi quet xong
+        try:
+            response = requests.delete(self.tarurl+"/api/v1/scans/"+str(scan_id),headers=self.headers,timeout=30,verify=False)
+            if response.status_code == "204":
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(str(e))
+            return
+    def checkProcess(self) : # kiem tra dang chay may tien trinh va xem cái nào đã xong thì report
         for scan_id in self.list_processing : 
             try:
                 response = requests.get(self.tarurl + '/api/v1/scans/' + str(scan_id), headers = self.headers, verify=False)
@@ -66,6 +110,7 @@ class myThread(threading.Thread):
                 print(result)
                 status = result['current_session']['status']
                 if status == "completed":
+                    self.getreports(scan_id)
                     self.list_processing.remove(scan_id)
             except Exception as e:
                 print(str(e))
